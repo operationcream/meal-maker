24214db8c6ed9963c57be80780742ebbbe61e75a
David <dlum988@gmail.com>
2019-02-28 18:24:11 -0600

    imported GroceryList and have it rendering on page

--- client/src/components/main/SavedRecipes.jsx
+++ client/src/components/main/SavedRecipes.jsx
@@ -1,6 +1,8 @@
 // a drawer (with a saved recipes list component and a saved recipe list item component)
 import React from 'react';
 import SavedRecipesList from './SavedRecipesList.jsx';
+import GroceryList from './GroceryList.jsx';
+import IngredientList from './IngredientList.jsx';
 
 const SavedRecipes = ({ savedRecipes, changeView, selectRecipe }) => (
 
@@ -8,6 +10,10 @@ const SavedRecipes = ({ savedRecipes, changeView, selectRecipe }) => (
     <h1>Your saved recipes</h1>
     <div>
       <SavedRecipesList savedRecipes={savedRecipes} changeView={changeView} selectRecipe={selectRecipe} />
+    </div>
+    <div>
+      <GroceryList />
+
     </div>
   </div>
 );
