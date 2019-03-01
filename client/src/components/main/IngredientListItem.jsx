c0b68d1fa17871fe10fd980a04bc707535a620c0
David <dlum988@gmail.com>
2019-02-28 19:17:54 -0600

    have ingredient name rendering

--- client/src/components/main/IngredientListItem.jsx
+++ client/src/components/main/IngredientListItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+// import Paper from '@material-ui/core/Paper';
 
-
-const IngredientListItem = props => (
-  <h2>ingredient list item</h2>
+const IngredientListItem = ({ingredient}) => (
+  <div>
+    {ingredient.name}
+  </div>
 );
 export default IngredientListItem;
