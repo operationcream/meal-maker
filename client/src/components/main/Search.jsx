8bca9f4cb986500d052fa79320b35789cd56f00b
lauraannpena <lauraannpena@gmail.com>
2019-02-28 18:47:01 -0600

    add modal for main page search

--- client/src/components/main/Search.jsx
+++ client/src/components/main/Search.jsx
@@ -1,35 +1,108 @@
 // component with an input form, a + button and a submit button
 
 import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import VideoPlayer from '../VideoPlayer.jsx';
 import RecipeInstructions from '../login/RecipeInstructions.jsx';
 import RecipeList from './RecipeList.jsx';
 import AutoComplete from './AutoComplete.jsx';
 
+const styles = theme => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+    width: 'fit-content',
+  },
+  formControl: {
+    marginTop: theme.spacing.unit * 2,
+    minWidth: 120,
+  },
+  // formControlLabel: {
+  //   marginTop: theme.spacing.unit,
+  // },
+});
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      open: false,
     };
+    this.handleClickSearchOpen = this.handleClickSearchOpen.bind(this);
+    this.handleSearchClose = this.handleSearchClose.bind(this);
+  }
+
+
+  handleClickSearchOpen() {
+    this.setState({ open: true });
+  }
+
+  handleSearchClose() {
+    this.setState({ open: false });
   }
 
   render() {
-    const { recipeOfTheDay, recipes, getRecipes, ingredients, saveRecipe, saveDislikeRecipe, selectRecipe, changeView, user } = this.props;
+    const {
+      classes,
+      handleClickSearchOpen,
+      handleSearchClose,
+      recipeOfTheDay,
+      recipes,
+      getRecipes,
+      ingredients,
+      saveRecipe,
+      saveDislikeRecipe, 
+      selectRecipe, 
+      changeView, 
+      user 
+    } = this.props;
+    const {
+      open,
+    } = this.state;
     return (
       <div>
         <h2 className="Lobster">
-          Welcome, 
-          {user}
+          Welcome,
+          {' '}
+          { user }
         </h2>
-        <h2 align="center">Search for a recipe</h2>
-        <div className="Search-Comp">
-          <AutoComplete ingredients={ingredients} addIngredient={this.addIngredient} getRecipes={getRecipes} />
-        </div>
+        <Button variant="outlined" color="primary" onClick={this.handleClickSearchOpen}>
+          Search Ingredients
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleSearchClose}
+          aria-labelledby="form-dialog-title"
+          maxWidth="lg"
+        >
+          <DialogTitle id="form-dialog-title">Search</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              What&apos;s in your fridge?
+            </DialogContentText>
+            <AutoComplete ingredients={ingredients} addIngredient={this.addIngredient} getRecipes={getRecipes} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleSearchClose} color="primary">
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        {/* <RecipeList recipes={recipes} saveRecipe={saveRecipe} saveDislikeRecipe={saveDislikeRecipe} selectRecipe={selectRecipe} changeView={changeView} /> */}
+
 
         <div className="test">
-          <RecipeList recipes={recipes} saveRecipe={saveRecipe} saveDislikeRecipe={saveDislikeRecipe} selectRecipe={selectRecipe} changeView={changeView}> </RecipeList>
+          <RecipeList recipes={recipes} saveRecipe={saveRecipe} saveDislikeRecipe={saveDislikeRecipe} selectRecipe={selectRecipe} changeView={changeView} />
         </div>
 
         <h3 id="recipeTitle">Recipe of the day</h3>
@@ -48,4 +121,5 @@ class Search extends React.Component {
   }
 }
 
-export default Search;
+// export default Search;
+export default withStyles(styles)(Search);
