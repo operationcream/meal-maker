7f3278c22546f264a159ee4e55421793fb956668
voodoochum <brian@aquavisionnola.com>
2019-03-01 18:45:40 -0600

    Merged

--- client/src/components/main/NutritionView.jsx
+++ client/src/components/main/NutritionView.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import {
   Card, withStyles, CardContent, Typography,
 } from '@material-ui/core';
+import Axios from 'axios';
 
 const styles = {
   card: {
@@ -22,6 +23,7 @@ const styles = {
   },
 };
 
+
 const Nutrition = ({ selectedRecipe, classes }) => {
   const ingredients = {};
   ingredients.name = selectedRecipe.singleIngredient[1].name;
@@ -29,88 +31,88 @@ const Nutrition = ({ selectedRecipe, classes }) => {
   ingredients.unit = selectedRecipe.singleIngredient[1].amount.us.unitShort;
   return (
     <Card className={classes.card}>
-    <CardContent>
-      <Typography variant="h5" component="h2">
-        <tr>
-          <td>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          <tr>
+            <td>
             Total Calories:
-          </td>
-          <td>
+            </td>
+            <td>
               500
-          </td>
-        </tr>
-      </Typography>
-      <Typography variant="h6">
-        <tr>
-          <td>
+            </td>
+          </tr>
+        </Typography>
+        <Typography variant="h6">
+          <tr>
+            <td>
             Protien:
-          </td>
-          <td />
-          <td>
-            {ingredients.amount}
-          </td>
-        </tr>
-      </Typography>
-      <Typography variant="h6">
-        <tr>
-          <td>
+            </td>
+            <td />
+            <td>
+              {ingredients.amount}
+            </td>
+          </tr>
+        </Typography>
+        <Typography variant="h6">
+          <tr>
+            <td>
             Total Fat:
-          </td>
-          <td />
-          <td>
+            </td>
+            <td />
+            <td>
            9g
-          </td>
-        </tr>
-      </Typography>
-      <Typography>
-        <tr>
-          <td />
-          <td>
+            </td>
+          </tr>
+        </Typography>
+        <Typography>
+          <tr>
+            <td />
+            <td>
             Sat Fat:
-          </td>
-          <td />
-          <td>
+            </td>
+            <td />
+            <td>
            2g
-          </td>
-        </tr>
-      </Typography>
-      <Typography variant="h6">
-        <tr>
-          <td>
+            </td>
+          </tr>
+        </Typography>
+        <Typography variant="h6">
+          <tr>
+            <td>
             Carbs:
-          </td>
-          <td />
-          <td>
+            </td>
+            <td />
+            <td>
             15g
-          </td>
-        </tr>
-      </Typography>
-      <Typography>
-        <tr>
-          <td />
-          <td>
+            </td>
+          </tr>
+        </Typography>
+        <Typography>
+          <tr>
+            <td />
+            <td>
             Sugar:
-          </td>
-          <td />
-          <td>
+            </td>
+            <td />
+            <td>
             5g
-          </td>
-        </tr>
-      </Typography>
-      <Typography>
-        <tr>
-          <td />
-          <td>
+            </td>
+          </tr>
+        </Typography>
+        <Typography>
+          <tr>
+            <td />
+            <td>
             Fiber:
-          </td>
-          <td />
-          <td>
+            </td>
+            <td />
+            <td>
             7g
-          </td>
-        </tr>
-      </Typography>
-    </CardContent>
-  </Card>
+            </td>
+          </tr>
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
