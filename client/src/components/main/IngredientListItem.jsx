19c41f7403ad99b5f0c3ed3491fb7536ba41c66e
David <dlum988@gmail.com>
2019-03-02 14:03:39 -0600

    have checkbox rendering dynamically and passing ingredient back up to savedRecipes

--- client/src/components/main/IngredientListItem.jsx
+++ client/src/components/main/IngredientListItem.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const state = {
-  checkedA: true,
-  checkedB: true,
-  checkedF: true,
-};
-let handleChange = name => (event) => {
-  this.setState({ [name]: event.target.checked });
-};
-
-handleChange = handleChange.bind(this);
-const IngredientListItem = ({ ingredient }) => (
+const IngredientListItem = ({ ingredient, onClick, checked }) => (
   <div>
     <Checkbox
-      checked={state.checkedA}
-      onChange={handleChange('checkedA')}
-      value="checkedA"
+      // checked={checked}
+      onClick={() => { onClick(ingredient); }}
+      // value="checkedA"
       color="primary"
     />
     {ingredient.name}
