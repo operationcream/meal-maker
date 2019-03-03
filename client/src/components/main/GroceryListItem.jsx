0ca2a8d89f352103fec3f8e3657493e99dba78af
David <dlum988@gmail.com>
2019-03-03 14:16:24 -0600

    have groceryListItem displaying the ingredient with amount

--- client/src/components/main/GroceryListItem.jsx
+++ client/src/components/main/GroceryListItem.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const GroceryListItem = ({ groceryItem }) => (
 
 
-  <div>
-    {groceryItem.name}
-  </div>
+  <table>
+    <tr>
+      <td>{groceryItem.amount.us.amount}</td>
+      <td>{groceryItem.amount.us.unitLong}</td>
+      <td>{groceryItem.name}</td>
+    </tr>
+  </table>
 );
 
 
